a409ef0d39d7d58f920acb071a9312f00bc33d7c
Jhovany <jhovany990@gmail.com>
2017-08-21 10:39:19 -0500

    first init

---
+++ chat_group/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chat_group
+//
+//  Created by Jhovany Gonzalez on 8/20/17.
+//  Copyright © 2017 Jhovany Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
